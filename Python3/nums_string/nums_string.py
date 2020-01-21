cc71f4be3e77af3722462439979669407830f057
nu11secur1ty <venvaropt@gmail.com>
2020-01-21 15:26:46 +0200

    Update nums_string.py

--- Python3/nums_string/nums_string.py
+++ Python3/nums_string/nums_string.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3.4
+# author @nu11secur1ty
 from num2words import num2words
 
+# Give a number
 print("Give a number")
 nums = input()
 
+# Give a language
+print("Lang")
+taeba = input()
+
 ke = num2words(nums)
 se = num2words(nums, to='ordinal')
-ge = num2words(nums, lang='pt')
+ge = num2words(nums, lang=taeba)
 
 print(ke , se  , ge)

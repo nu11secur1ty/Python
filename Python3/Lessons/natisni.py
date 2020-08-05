073aebc7edad45d7e184bb5d3e38047956fe38d3
nu11secur1ty <venvaropt@gmail.com>
2020-08-05 09:26:44 +0300

    Update natisni.py

--- Python3/Lessons/natisni.py
+++ Python3/Lessons/natisni.py
@@ -61,3 +61,17 @@ print("### User input\n")
 kuvsitive = input("Enter kuvsitive:")
 print("Ti si: " + kuvsitive)
 print("\n")
+
+### Recursive
+print("### Recursive\n")
+def factorial(n):
+    print("factorial has been called with n = " + str(n))
+    if n == 1:
+        return 1
+    else:
+        res = n * factorial(n-1)
+        print("intermediate result for ", n, " * factorial(" ,n-1, "): ",res)
+        return res	
+
+print(factorial(5))
+print("\n")
